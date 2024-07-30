51f673412c316edc3cf0942c3948efd44129a1b4
L Mario <luismariovasquezcorrea@gmail.com>
2024-07-30 17:17:19 -0500

    Complete cli.py code

--- calculator/cli.py
+++ calculator/cli.py
@@ -8,5 +8,54 @@ from calculator.model import Calculator
 def calc(ctx: click.Context):
     """ A simple calculator"""
 
-    ctx.obj - {"calculator_object", Calculator()}
+    ctx.obj = {"calculator_object", Calculator()}
+
+
+@click.command()
+@click.argument('num1', type=float)
+@click.argument('num2', type=float)
+@click.pass_context
+def add(ctx: click.Context, num1: float, num2: float) -> None:
+    """Add two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.add(num1, num2)
+    click.echo(result)
+
+
+@click.command()
+@click.argument('num1', type=float)
+@click.argument('num2', type=float)
+@click.pass_context
+def subtract(ctx: click.Context, num1: float, num2: float) -> None:
+    """Add two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.subtract(num1, num2)
+    click.echo(result)
+
+
+@click.command()
+@click.argument('num1', type=float)
+@click.argument('num2', type=float)
+@click.pass_context
+def multiply(ctx: click.Context, num1: float, num2: float) -> None:
+    """Multiply two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.multiply(num1, num2)
+    click.echo(result)
     
+
+@click.command()
+@click.argument('num1', type=float)
+@click.argument('num2', type=float)
+@click.pass_context
+def divide(ctx: click.Context, num1: float, num2: float) -> None:
+    """Divide two numbers."""
+    calculator = ctx.obj["calculator_object"]
+    result = calculator.divide(num1, num2)
+    click.echo(result)
+    
+
+calc.add_command(add)
+calc.add_command(subtract)
+calc.add_command(multiply)
+calc.add_command(divide)
